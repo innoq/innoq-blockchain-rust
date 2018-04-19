34bff5bcd196d7eac6baea43e1f1d6a5a94ecc72
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2018-04-19 15:38:31 +0200

    ToJSON impl for Block fixes + test

--- src/to_json.rs
+++ src/to_json.rs
@@ -30,13 +30,13 @@ impl<'a> ToJSON for &'a str {
     }
 }
 
-impl<T> ToJSON for Vec<T>
+impl<'a, T> ToJSON for &'a Vec<T>
 where
     T: ToJSON,
 {
     fn to_json(&self) -> String {
         let mut acc = "[".to_string();
-        for elem in self {
+        for elem in self.iter() {
             acc += &elem.to_json();
             acc += ",";
         }
@@ -63,7 +63,7 @@ impl ToJSON for Block {
         add_property(&mut json, "index", self.index, false);
         add_property(&mut json, "timestamp", self.timestamp, false);
         add_property(&mut json, "proof", self.proof, false);
-        add_property(&mut json, "transactions", self.transactions, false);
+        add_property(&mut json, "transactions", &self.transactions, false);
         add_property(&mut json, "previousBlockHash", self.previous_block_hash.as_str(), false);
         json.push_str("}");
         json
@@ -74,7 +74,7 @@ fn add_property<V>(json: &mut String, name: &str, value: V, last: bool)
     where V: ToJSON {
     json.push_str("\"");
     json.push_str(name);
-    json.push_str(":\"");
+    json.push_str("\":");
     json.push_str(value.to_json().as_str());
     if !last {
         json.push_str(",");
@@ -94,5 +94,16 @@ fn it_works() {
             timestamp: 1
         }.to_json(),
         r#"{"id":"\"","timestamp":1,"payload":"a"}"#);
-    assert_eq!(vec![1 as u64, 2, 3].to_json(), "[1,2,3]");
+    assert_eq!((&vec![1 as u64, 2, 3]).to_json(), "[1,2,3]");
+    assert_eq!(Block{
+        index: 1,
+        timestamp: 0,
+        proof: 1917336,
+        transactions: vec![Transaction{
+            id: "b3c973e2-db05-4eb5-9668-3e81c7389a6d".to_string(),
+            timestamp: 0,
+            payload: "I am Heribert Innoq".to_string(),
+        }],
+        previous_block_hash: "0".to_string(),
+    }.to_json(), "{\"index\":1,\"timestamp\":0,\"proof\":1917336,\"transactions\":[{\"id\":\"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\":0,\"payload\":\"I am Heribert Innoq\"}],\"previousBlockHash\":\"0\"}");
 }
