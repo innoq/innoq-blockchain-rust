30012b994b88fc00c28c40a5e0bbda86ce3e5ed6
Daniel Bornkessel <daniel.bornkessel@innoq.com>
2018-04-19 14:49:37 +0200

    Epic commit!

--- src/block.rs
+++ src/block.rs
@@ -1,4 +1,4 @@
-pub struct Blocks {
+pub struct Blockchain {
     pub blocks: Vec<Block>,
     pub block_height: u64,
 }
@@ -15,4 +15,33 @@ pub struct Transaction {
     pub id: String,
     pub timestamp: u64,
     pub payload: String,
+}
+
+impl Blockchain {
+    pub fn new() -> Blockchain {
+        return Blockchain {
+            block_height:  1,
+            blocks: vec![
+                Block{
+                    index: 1,
+                    timestamp: 0,
+                    proof: 1917336,
+                    transactions: vec![
+                        Transaction{
+                            id: String::from("b3c973e2-db05-4eb5-9668-3e81c7389a6d"),
+                            timestamp: 0,
+                            payload: String::from("I am Heribert Innoq"),
+                        }
+                    ],
+                    previous_block_hash: String::from("0"),
+                }
+            ]
+        }
+    }
+}
+
+#[test]
+fn constructor_creates_correct_genesis_block() {
+    let blockchain = Blockchain::new();
+    assert_eq!(blockchain.blocks[0].proof, 1917336);
 }
