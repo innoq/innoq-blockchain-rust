062626718632d8df0d77c37c7124aeac4a702615
Kofi Jedamzik <>
2018-04-19 12:17:29 +0200

    add web stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+extern crate gotham;
+extern crate hyper;
+extern crate mime;
+
+use hyper::{Response, StatusCode};
+
+use gotham::http::response::create_response;
+use gotham::state::State;
+
+pub fn say_hello(state: State) -> (State, Response) {
+    let res = create_response(
+        &state,
+        StatusCode::Ok,
+        Some((String::from("Hello World!").into_bytes(), mime::TEXT_PLAIN)),
+    );
+
+    (state, res)
+}
+
+pub fn main() {
     println!("Hello, world!");
+
+    let addr = "127.0.0.1:7878";
+
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, || Ok(say_hello))
 }
