f9cae6ce26572df53c0a272e3770899cc6d66136
Jan Stępień <jan.stepien@innoq.com>
2018-04-20 11:38:21 +0200

    Inject state via a middleware

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
+extern crate crypto_hash;
+extern crate futures;
 extern crate gotham;
 extern crate hyper;
 extern crate mime;
-extern crate futures;
-extern crate crypto_hash;
 extern crate rayon;
 
 use std::time::SystemTime;
 use std::sync::{Arc, Mutex};
 
-
 mod to_json;
 mod block;
 mod calculate_proof;
 
-use hyper::{Response, StatusCode, Method};
+use hyper::{Method, Response, StatusCode};
 use gotham::handler::{Handler, HandlerFuture, NewHandler};
 use futures::future;
 
+use gotham::middleware::{Middleware, NewMiddleware};
 use gotham::http::response::create_response;
 use gotham::router::builder::*;
 use gotham::router::Router;
+use gotham::pipeline::new_pipeline;
+use gotham::pipeline::single::single_pipeline;
 use block::{Blockchain, Transaction};
 use to_json::ToJSON;
-use gotham::state::{FromState, State};
+use gotham::state::{FromState, State, StateData};
 
 #[derive(Debug)]
 struct ServerState {
@@ -47,7 +49,6 @@ impl GetBlocksHandler {
 
 impl Handler for GetBlocksHandler {
     fn handle(self, state: State) -> Box<HandlerFuture> {
-
         let mut status_code = StatusCode::Ok;
 
         let mut blockchain = &self.state.lock().unwrap().blockchain;
@@ -71,7 +72,9 @@ impl NewHandler for GetBlocksHandler {
     }
 }
 
-pub fn say_hello(state: State) -> (State, Response) {
+pub fn say_hello(mut state: State) -> (State, Response) {
+    let mut server_state = state.borrow_mut::<ExampleMiddlewareData>().state.clone();
+    eprintln!("{:?}", server_state.lock().unwrap().blockchain);
     let res = create_response(
         &state,
         StatusCode::Ok,
@@ -81,16 +84,52 @@ pub fn say_hello(state: State) -> (State, Response) {
     (state, res)
 }
 
+#[derive(Clone)]
+pub struct ExampleMiddleware {
+    state: Arc<Mutex<ServerState>>,
+}
+
+impl NewMiddleware for ExampleMiddleware {
+    type Instance = Self;
+
+    fn new_middleware(&self) -> std::io::Result<Self::Instance> {
+        Ok(self.clone())
+    }
+}
+
+pub struct ExampleMiddlewareData {
+    state: Arc<Mutex<ServerState>>,
+}
+
+impl StateData for ExampleMiddlewareData {}
+
+impl Middleware for ExampleMiddleware {
+    fn call<Chain>(self, mut state: State, chain: Chain) -> Box<HandlerFuture>
+    where
+        Chain: FnOnce(State) -> Box<HandlerFuture>,
+    {
+        state.put(ExampleMiddlewareData {
+            state: self.state.clone(),
+        });
+
+        chain(state)
+    }
+}
+
 fn router() -> Router {
     let state = Arc::new(Mutex::new(ServerState {
         blockchain: Blockchain::new(),
         candidates: Vec::new(),
     }));
+    let mw = ExampleMiddleware { state: state.clone() };
+    let (chain, pipelines) = single_pipeline(new_pipeline().add(mw).build());
 
-    build_simple_router(|route| {
+    build_router(chain, pipelines, |route| {
         route.get("/").to(say_hello);
         route.get("/mine").to(say_hello);
-        route.get("/blocks").to_new_handler(GetBlocksHandler::new(state.clone()));
+        route
+            .get("/blocks")
+            .to_new_handler(GetBlocksHandler::new(state.clone()));
     })
 }
 
